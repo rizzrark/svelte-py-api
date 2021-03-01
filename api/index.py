9be89c87d12da636f030d0ce20f699d1bce00098
rizzrark <razzilsp@gmail.com>
2021-03-01 00:01:03 -0500

    update py req

--- api/index.py
+++ api/index.py
@@ -4,6 +4,7 @@ from cowpy import cow
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
