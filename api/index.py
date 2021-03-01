f7b37ebc12ec4b00da2e448f03946e6e71f46966
rizzrark <razzilsp@gmail.com>
2021-03-01 00:19:34 -0500

    api test

--- api/index.py
+++ api/index.py
@@ -4,10 +4,18 @@ from cowpy import cow
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        covid = Covid()
+        covid = Covid(source="worldometers")
+        cuba = covid.get_status_by_country_name("cuba")
         
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
         message = cow.Cowacter().milk('Hello from Python from a Serverless Function!')
-        self.wfile.write(message.encode())
+        self.wfile.write(cuba.encode())
         return
+
+        from covid import Covid
+
+
+
